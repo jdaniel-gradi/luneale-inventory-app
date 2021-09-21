10d5d7867bb5d27ee74d43c92489dbf8fa7bb445
Daniel Saavedra <danielsaavedram@hotmail.com>
2021-09-21 07:56:50 -0500

    Refactored inventory-reducing function and got it to return tax payments

--- services/products/product.service.js
+++ services/products/product.service.js
@@ -3,8 +3,11 @@ require("dotenv").config();
 const {
     getProductById,
     getProductByHandle,
+    getVariant,
     getVariantBySKU
 } = require("../../graphql/products/queries/product.query");
+
+const { adjustInventoryQuantity } = require("../../graphql/inventory/mutations/inventory.mutation");
 
 const graphQLClient = require("../../graphql");
 
@@ -61,6 +64,7 @@ class ProductService {
 
                 data.push({
                     bundleDetails: {
+                        duo: product.tags.includes("pack|duo"),
                         product: {
                             title: product.title,
                             id: product.id
@@ -84,9 +88,67 @@ class ProductService {
         return data;
     }
 
+    async reduceInventory(id, quantity) {
+        const response = await graphQLClient.request(adjustInventoryQuantity, {
+            input: {
+                inventoryLevelId: id,
+                availableDelta: parseInt(quantity)
+            }
+        });
+
+        return response;
+    }
+
     async reduceBundleInventories(arr) {
-        for (const subProd of arr) {
+        const taxRules = [];
+        let processedCup = false;
+
+        for (const bundle of arr) {
+            const { quantity, bundleDetails } = bundle;
+            for (const subProd of bundle.subproducts) {
+                let variantTitle;
+
+                let response = await this.getProductByHandle(subProd.handle);
+
+                const product = response.product;
+
+                if (bundle.duo && subProd.handle.includes("cup")) {
+                    const arr = bundleDetails.variant.title.split("/").map(el => el.trim());
+                    variantTitle = processedCup ? arr[1] : arr[0];
+                } else {
+                    variantTitle = bundle.variant.title;
+                }
+
+                const variant = product.variants.edges
+                    .map(el => el.node)
+                    .find(el => el.title == variantTitle);
+
+                if (!variant) return;
+
+                // Extract product VAT rate from metafields
+                const VAT = product.metafields.edges
+                    .map(el => el.node)
+                    .find(el => el.namespace == "my_fields" && el.key == "vat").value;
+
+                taxRules.push({
+                    productId: product.id,
+                    VAT: parseFloat(VAT),
+                    price: parseFloat(subProd.price),
+                    quantity
+                });
+
+                const invLevelId = variant.inventoryItem.inventoryLevels.edges.find(
+                    el => el.node.available > 0
+                ).node.id;
+
+                response = await this.reduceInventory(invLevelId, quantity);
+                console.log(
+                    `Reduced inventory of variant ${subProd.bundleDetails.variant.title} from product ${subProd.bundleDetails.product.title} by ${quantity}`
+                );
+            }
         }
+
+        return taxRules;
     }
 }
 

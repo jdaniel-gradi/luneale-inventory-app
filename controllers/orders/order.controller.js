12f0f4e6f0e1e60679aaa0edd17f68351977ccd7
Daniel Saavedra <danielsaavedram@hotmail.com>
2021-09-21 20:30:58 -0500

    Added accumulation of non-bundle line-items to tax object

--- controllers/orders/order.controller.js
+++ controllers/orders/order.controller.js
@@ -13,7 +13,7 @@ class Order {
     async handleNewOrder(req, res) {
         if (!req.body) return res.sendStatus(200);
 
-        const { id: orderId, line_items, email } = req.body;
+        const { id: orderId, line_items, email, order_number: orderNumber } = req.body;
 
         // ! Only moves forward if customer email address has a gradiweb.com domain (remove before going live!!!!)
 
@@ -38,8 +38,10 @@ class Order {
 
             // Reduce function returns array of objects with productId, tax rate and provided price (product metafield) for further processing
             console.log(response);
+
             // Aggregate tax rules array into object with unique rates as keys
             const taxes = {};
+
             for (const rule of response) {
                 const { price, quantity } = rule;
                 const VAT = rule.VAT / 100;
@@ -47,7 +49,34 @@ class Order {
                 taxes[VAT] = taxes[VAT] || 0;
                 taxes[VAT] += ((price / (1 + VAT)) * quantity) / VAT;
             }
+
             console.log(taxes);
+
+            // In addition to bundle products, other line_items should have their taxes added
+            const bundleProductIds = pendingMods.map(el => {
+                // Might be in GQL format
+                let bundleId = el.bundleDetails.product.id;
+                if (bundleId.includes("/")) bundleId = bundleId.split("/");
+                const productId = typeof bundleId == "string" ? bundleId : slice(-1)[0];
+
+                return productId;
+            });
+
+            const nonBundleItems = line_items.filter(
+                el => !bundleProductIds.includes(el.product_id)
+            );
+
+            console.log("Found non-bundle items in order");
+
+            if (nonBundleItems.length) {
+                for (const item of nonBundleItems) {
+                    for (const taxLine of item.tax_lines) {
+                        taxes[taxLine.rate] = taxes[taxLine.rate] || 0;
+                        taxes[taxLine.rate] += parseFloat(taxLine.price);
+                    }
+                }
+            }
+
             // Create note in order that lays out collected info
             let note = "Collected taxes:\n";
 
@@ -57,7 +86,7 @@ class Order {
 
             response = await orderServiceInstance.updateOrderNote(orderId, note);
 
-            console.log("Updated order note");
+            console.log(`Added the following note to order #${orderNumber}:`);
             console.log(note);
         } catch (err) {
             console.error(err);

367ed6902fe124a68f2f36591ed65d36d39773e8
Daniel Saavedra <jdaniel.saavedra@gradiweb.com>
2021-09-29 06:44:43 -0500

    Added new endpoint for order-cancelled webhook

--- routes/webhooks/orders/order.route.js
+++ routes/webhooks/orders/order.route.js
@@ -5,5 +5,6 @@ const OrderController = require("../../../controllers/orders/order.controller");
 const orderController = new OrderController();
 
 router.post("/order-created", orderController.handleNewOrder);
+router.post("/order-cancelled", orderController.handleCancelledOrder);
 
 module.exports = router;
